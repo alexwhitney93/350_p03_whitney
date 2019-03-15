b7c7134ee7ab4731a77d0ff896c1bb166582a098
alexwhitney90 <alexwhitney90@gmail.com>
2019-03-15 04:21:03 -0400

    initial commit

---
+++ CacheSimulator.cpp
@@ -0,0 +1,18 @@
+// no locality (Figure 22.6) 100 unique pages, 10000 page accesses, next page chosen is completely random
+
+
+
+// 80-20 (Figure 22.8) 100 unique pages, 10000 page accesses, 80% of references come from 20% of the pages, other 20% come from the other 80% of the pages
+
+
+
+// looping (Figure 22.9) 
+
+
+
+// simulate:
+// ORT - replace the page that has the longest time until next use
+// LRU - replace the page that was least recently used
+// FIFO - replace the page that was first into the cache
+// Rand - replace a random page
+// Clock - 
